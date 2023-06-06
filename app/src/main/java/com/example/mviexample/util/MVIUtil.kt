0b39e7ce070885e56589edfd7f9606cac4298663
osemin <semin015123@gmail.com>
2023-06-06 23:17:27 +0900

    fix(MVIUtil) : edit parameter name

--- app/src/main/java/com/example/mviexample/util/MVIUtil.kt
+++ app/src/main/java/com/example/mviexample/util/MVIUtil.kt
@@ -5,6 +5,6 @@ import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.*
 
 
-fun <Event, State> Channel<Event>.channelToStateFlow(state: State, lambda: (State, Event) -> State, scope: CoroutineScope): StateFlow<State> {
-    return receiveAsFlow().runningFold(state, lambda).stateIn(scope, SharingStarted.Eagerly, state)
+fun <Event, State> Channel<Event>.channelToStateFlow(state: State, block: (State, Event) -> State, scope: CoroutineScope): StateFlow<State> {
+    return receiveAsFlow().runningFold(state, block).stateIn(scope, SharingStarted.Eagerly, state)
 }
