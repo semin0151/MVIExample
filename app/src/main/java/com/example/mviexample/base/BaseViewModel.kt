8c0dcde7f16a70a8d3cd9f75e700738ec9146736
osemin <semin015123@gmail.com>
2023-07-09 18:02:46 +0900

    add(BaseViewModel) : add SideEffect

--- app/src/main/java/com/example/mviexample/base/BaseViewModel.kt
+++ app/src/main/java/com/example/mviexample/base/BaseViewModel.kt
@@ -2,12 +2,15 @@ package com.example.mviexample.base
 
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
 
-abstract class BaseViewModel<State, Event>: ViewModel() {
+abstract class BaseViewModel<State, Event, SideEffect>: ViewModel() {
     protected abstract var currentEvent: Event
     protected val events = Channel<Event>()
+    protected val sideEffects = Channel<SideEffect>()
     abstract val state: StateFlow<State>
+    abstract val sideEffect: Flow<SideEffect>
 
     protected suspend fun onEvent(event: Event) {
         currentEvent = event
@@ -15,4 +18,8 @@ abstract class BaseViewModel<State, Event>: ViewModel() {
     }
 
     protected abstract fun changeState(current: State, event: Event): State
+
+    protected suspend fun onSideEffect(sideEffect: SideEffect) {
+        sideEffects.send(sideEffect)
+    }
 }
