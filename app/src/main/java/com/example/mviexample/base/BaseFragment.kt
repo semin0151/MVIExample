e6ccefaa6d7caf86fce6b4b93947f5103b09647f
osemin <semin015123@gmail.com>
2023-07-09 17:49:32 +0900

    add(BaseFragment) : add SideEffect

--- app/src/main/java/com/example/mviexample/base/BaseFragment.kt
+++ app/src/main/java/com/example/mviexample/base/BaseFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
@@ -12,14 +13,19 @@ import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-abstract class BaseFragment<Binding: ViewDataBinding, State, Event>(@LayoutRes val layoutId: Int): Fragment() {
+abstract class BaseFragment<Binding : ViewDataBinding, State, Event, SideEffect>(@LayoutRes val layoutId: Int) :
+    Fragment() {
 
-    abstract val viewModel: BaseViewModel<State, Event>
+    abstract val viewModel: BaseViewModel<State, Event, SideEffect>
 
     private var _binding: Binding? = null
     private val binding: Binding get() = _binding!!
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         _binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
         return binding.root
     }
@@ -48,4 +54,9 @@ abstract class BaseFragment<Binding: ViewDataBinding, State, Event>(@LayoutRes v
     abstract fun initData(bundle: Bundle?)
     abstract fun initView()
     protected open fun render(state: State) {}
+    protected open fun handleSideEffect(sideEffect: SideEffect) {}
+
+    protected fun makeToast(text: String, length: Int = Toast.LENGTH_SHORT) {
+        Toast.makeText(requireContext(), text, length).show()
+    }
 }
